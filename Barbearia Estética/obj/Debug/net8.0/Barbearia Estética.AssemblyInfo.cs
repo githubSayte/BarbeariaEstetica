5c6c442b53dd10db3ea5601a6575fc452038eaef
Hokague <criativasayte@gmail.com>
2024-12-18 11:47:35 -0300

    Up versão 12

--- Barbearia Estética/obj/Debug/net8.0/Barbearia Estética.AssemblyInfo.cs
+++ Barbearia Estética/obj/Debug/net8.0/Barbearia Estética.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Barbearia Estética")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+33fe1d82aab9f4a9f7e6f39735aa7b9365f110e5")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1828a8f3990ee89c9ad27969394068b40c5f8c6d")]
 [assembly: System.Reflection.AssemblyProductAttribute("Barbearia Estética")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Barbearia Estética")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
